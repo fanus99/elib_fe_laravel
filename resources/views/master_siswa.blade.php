ff7e21242888e29fccb2a89d56a7cda60e5090c2
fanus99 <fajaryunus99@gmail.com>
2023-05-25 22:10:26 +0700

    merge

--- resources/views/master_siswa.blade.php
+++ resources/views/master_siswa.blade.php
@@ -87,6 +87,23 @@
                 </footer>
             </div>
         </div>
+        <div class="modal" id="ModalCreate" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> -->
         <script src="{{asset('js/scripts.js')}}"></script>
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script> -->
@@ -95,7 +112,6 @@
         <!-- <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script> -->
         <!-- <script src="{{asset('js/datatables-simple-demo.js')}}"></script> -->
         <script type="module">
-
             var $baseroute = "/master/siswa/api/";
 
             function getData(){
@@ -141,9 +157,29 @@
                     serverSide: false
                 });
             }
+            // $('#ModalCreate').modal('show');
+            var exampleModal = document.getElementById('ModalCreate')
+            exampleModal.addEventListener('show.bs.modal', function (event) {
+            // Button that triggered the modal
+            var button = event.relatedTarget
+            // Extract info from data-bs-* attributes
+            var recipient = button.getAttribute('data-bs-whatever')
+            // If necessary, you could initiate an AJAX request here
+            // and then do the updating in a callback.
+            //
+            // Update the modal's content.
+            var modalTitle = exampleModal.querySelector('.modal-title')
+            var modalBodyInput = exampleModal.querySelector('.modal-body input')
 
+            modalTitle.textContent = 'New message to ' + recipient
+            modalBodyInput.value = recipient
+            })
             $(function() {
-                getDataById(4);
+                // $("ModalCreate").show()
+
+
+                getData();
+                // getDataById(4);
             });
 
 
